137b803217f565643053b563293f85504d2bb187
tomaszpaloc <132575+kaff@users.noreply.github.com>
2020-12-05 19:07:28 +0100

    Added model currency validation

--- src/ProductsCatalog/Domain/Currency.php
+++ src/ProductsCatalog/Domain/Currency.php
@@ -11,11 +11,19 @@ final class Currency
 
     public function __construct(string $currencyCode)
     {
+        $this->guardCurrencyCode($currencyCode);
         $this->currencyCode = $currencyCode;
     }
 
     public function __toString()
     {
         return $this->currencyCode;
+    }
+
+    private function guardCurrencyCode(string $currencyCode)
+    {
+        if (strlen($currencyCode) !== 3) {
+            throw new \InvalidArgumentException('Invalid currency code. Currency code must be 3 characters long.');
+        }
     }
 }
